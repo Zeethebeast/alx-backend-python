72313855982ab9a5adb37cdd6bd22fc52e81684b
zeethebeast <chimasamuel6@gmail.com>
2024-10-14 18:00:54 +0100

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """wait_n concurrence"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
