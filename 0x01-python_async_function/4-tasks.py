4122be4b943896f840335abd4f3dcee78e6c05f0
zeethebeast <chimasamuel6@gmail.com>
2024-10-14 17:53:29 +0100

    tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Import wait_random from the previous python file"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n concurrence"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*tasks)
+    result = sorted(result, key=float)
+    """You will spawn wait_random n times with the specified max_delay."""
+    return result
